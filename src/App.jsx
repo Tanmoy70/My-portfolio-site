0599e76403ef4ca5274c6592150e7b326184eb6b
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-09-25 23:57:10 +0530

    make comment better

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
           <HomeSkeleton />
         </main>
 
-        {/* Footer Skeleton */}
+        {/* Footer Skeleton as footer visible first */}
         <footer className="footer">
           <div className="footer_container container">
             <div style={{ textAlign: 'center', padding: '2rem 0' }}>
